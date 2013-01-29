1e3e65e51228250089c0674d35207d6dcf924042
Sempr <iamsempr@gmail.com>
2013-01-30 00:11:40 +0800

    MOD: fix R0-C bug

--- R0_C.c
+++ R0_C.c
@@ -7,40 +7,34 @@ using namespace std;
 int k,n,a,b,c,r,K;
 int m[200200];
 int cnt[100100];
-set<int> mm;
-set<int>::iterator ii;
 
 int solve(){
     int i,j,t,e;
     scanf("%d%d",&n,&k);
     scanf("%d%d%d%d",&a,&b,&c,&r);
-    mm.clear();
     m[0]=a;
-    for (i=1;i<k;i++)
-        m[i] = int((1LL*b*m[i-1]+c)%r);
+    for (i=1;i<k;i++) m[i] = int((1LL*b*m[i-1]+c)%r);
     memset(cnt,0,sizeof(cnt));    
     for (i=0;i<k;i++){
         if (m[i]<=k){
             cnt[m[i]]++;
         }
     }
-    for (i=0;i<=k;i++) 
-        if (cnt[i]==0) mm.insert(i);
-    K = k+k+20;
-    for (i=k;i<=K;i++){
-        ii = mm.begin();
-        m[i] = *ii;
-        t = *ii;
-        cnt[t]++;
-        mm.erase(ii);
-        j = m[i-k];
-        if (j<=k){
-            cnt[j]--;
-            if (cnt[j]==0)
-                mm.insert(j);
-        }
-        //printf("m[%d]=%d\n",i,m[i]);
+    for (j=0;j<=k;j++) if (cnt[j]==0){
+        m[k]=j; cnt[j]++;
+        break;
     }
+    K = k+k; t = 0; j++;
+    for (i=k+1;i<=K;i++,t++){
+        if (m[t]<=k) cnt[m[t]]--;
+        while (cnt[j]>0) j++;
+        if (m[t]<=j && cnt[m[t]]==0) m[i]=m[t];
+        else{
+            m[i]=j; j++;
+        }
+        cnt[m[i]]++;
+    }
+    //for (i=0;i<=K;i++) printf("m[%d]=%d\n",i,m[i]);
     // circle k+1, first m[k];
     return m[k+(n-k-1)%(k+1)];
 }
